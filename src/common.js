4f089fc494a2961cc4c4b1b6c14c5db49b73e791
Chris Porter <chris.porter@um.edu.mt>
2024-11-06 08:54:11 +0100

    RefreshRate calculation bug - samples.length

--- src/common.js
+++ src/common.js
@@ -1,6 +1,6 @@
 import Decimal from './external/decimal.mjs';
 
-export const calculateCycleDurationInSeconds = frequency => 1 / frequency; 
+export const calculateCycleDurationInSeconds = frequency => 1 / frequency;
 
 
 // --------------------- Refresh Rate Helpers ---------------------
@@ -13,30 +13,30 @@ export const getRefreshRateReadings = async (bufferSize = 10, samples = 10) => {
   return new Promise(resolve => {
 
     const run = (now) => {
-        rafTimestamps.unshift(now); // place current timestamp (passed each time requestAnimationFrame is invoked) at the start of the array
-  
-        if (rafTimestamps.length > bufferSize) { // once 'timestamps' array contains 11 elements
-    
-            var firstTime = rafTimestamps.pop();
-            var refreshRate = 1000 * bufferSize / (now - firstTime);
-            refreshRates.unshift(refreshRate);
-        
-            if (refreshRates.length == samples) {
-                cancelAnimationFrame(id);
-                resolve(refreshRates)
-                return;
-            }
+      rafTimestamps.unshift(now); // place current timestamp (passed each time requestAnimationFrame is invoked) at the start of the array
+
+      if (rafTimestamps.length > bufferSize) { // once 'timestamps' array contains 11 elements
+
+        var firstTime = rafTimestamps.pop();
+        var refreshRate = 1000 * bufferSize / (now - firstTime);
+        refreshRates.unshift(refreshRate);
+
+        if (refreshRates.length == samples.length) {
+          cancelAnimationFrame(id);
+          resolve(refreshRates)
+          return;
         }
-        
-        var id = window.requestAnimationFrame(run);
-    }
+      }
 
       var id = window.requestAnimationFrame(run);
+    }
+
+    var id = window.requestAnimationFrame(run);
   })
 
 };
 
-export const calculateRefreshRate = async (bufferSize=10, samples=10) => {
+export const calculateRefreshRate = async (bufferSize = 10, samples = 10) => {
 
   const refreshRates = await getRefreshRateReadings(bufferSize, samples)
 
@@ -54,77 +54,69 @@ export const calculateRefreshRate = async (bufferSize=10, samples=10) => {
 
   if (maxCount.length > 1) maxCount = [maxCount.reduce((a, b) => a + b) / maxCount.length];
 
-  const val = Number(maxCount.shift().toFixed(7)) 
-  return val  
+  const val = Number(maxCount.shift().toFixed(7))
+  return val
 }
 
 
 // --------------------- Decimal.js Helpers ---------------------
 export const period = new Decimal(2).times(Decimal.acos(-1));
 
-export function countDecimals(stimulusFrequency) 
-{
-    if (Math.floor(stimulusFrequency) === Number(stimulusFrequency)) 
-          return 0;
+export function countDecimals(stimulusFrequency) {
+  if (Math.floor(stimulusFrequency) === Number(stimulusFrequency))
+    return 0;
 
-    var noOfDecimalPlaces = stimulusFrequency.toString().split(".")[1].length;
+  var noOfDecimalPlaces = stimulusFrequency.toString().split(".")[1].length;
 
-    if (noOfDecimalPlaces < 3)
-      return noOfDecimalPlaces; 
+  if (noOfDecimalPlaces < 3)
+    return noOfDecimalPlaces;
 
-    throw "Stimuli frequencies must have less than 3 decimal places."
+  throw "Stimuli frequencies must have less than 3 decimal places."
 }
 
-export function calculateNumberOfSeconds(stimulusFrequency)
-{
-    var maxSeconds = Math.pow(10, countDecimals(stimulusFrequency));
+export function calculateNumberOfSeconds(stimulusFrequency) {
+  var maxSeconds = Math.pow(10, countDecimals(stimulusFrequency));
 
-    for (var noOfSecs = 1; noOfSecs <= maxSeconds; noOfSecs += 1) 
-    {
-          
-        var noOfCycles = new Decimal(noOfSecs).times(stimulusFrequency);
+  for (var noOfSecs = 1; noOfSecs <= maxSeconds; noOfSecs += 1) {
 
-        if (noOfCycles.mod(1).toNumber() === 0)
-           return noOfSecs;
-    }
+    var noOfCycles = new Decimal(noOfSecs).times(stimulusFrequency);
 
-    throw "Failed to calculate the required number of seconds.";
+    if (noOfCycles.mod(1).toNumber() === 0)
+      return noOfSecs;
+  }
+
+  throw "Failed to calculate the required number of seconds.";
 }
 
-export function modulus(x, y)
-{
-    var quotient = x.div(y);
+export function modulus(x, y) {
+  var quotient = x.div(y);
 
-    var quotientInt = new Decimal(quotient.toFixed(0));
+  var quotientInt = new Decimal(quotient.toFixed(0));
 
-    if ((quotient.minus(quotientInt)).div(quotientInt).abs().toNumber() < Number.EPSILON)
-    {
-      return new Decimal(0);
+  if ((quotient.minus(quotientInt)).div(quotientInt).abs().toNumber() < Number.EPSILON) {
+    return new Decimal(0);
+  }
+  else {
+    var n = quotient.floor();
+    var temp = y.times(n);
+
+    var signY = Math.sign(y.toNumber());
+    var result = x.minus(temp);
+
+    if (!x.equals(y) && !result.equals(0)) {
+      if (Math.sign(result.toNumber()) != signY)
+        result = result.abs().times(signY);
     }
-    else
-    {
-      var n = quotient.floor();
-      var temp = y.times(n);
 
-      var signY = Math.sign(y.toNumber());
-      var result = x.minus(temp);
-
-      if (!x.equals(y) && !result.equals(0))
-      {
-        if (Math.sign(result.toNumber()) != signY)
-          result = result.abs().times(signY);
-      }
-
-      return result;
-    }
+    return result;
+  }
 }
 
-export function generateSquareWave(timeInstant)
-{
-	const dutyCycle = new Decimal(50), 
-		  halfPeriod = period.times(dutyCycle.div(100));
-		  
-	var modulusValue = modulus(timeInstant, period);
+export function generateSquareWave(timeInstant) {
+  const dutyCycle = new Decimal(50),
+    halfPeriod = period.times(dutyCycle.div(100));
 
-	return 2 * (modulusValue.toNumber() < halfPeriod.toNumber() ? 1 : 0) - 1;
+  var modulusValue = modulus(timeInstant, period);
+
+  return 2 * (modulusValue.toNumber() < halfPeriod.toNumber() ? 1 : 0) - 1;
 }
